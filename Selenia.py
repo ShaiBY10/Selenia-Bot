1ce39a4a3532f59c9d96352bf6901e35a19c9a39
ShaiBY10 <Shaibenyosef1@gmail.com>
2022-02-23 19:38:49 +0200

    Minor improvements

--- Selenia.py
+++ Selenia.py
@@ -4,16 +4,10 @@ from random import choice
 from helpcog import HelpCog
 from musiccog import MusicCog
 
-
 bot = commands.Bot(command_prefix="!")
 bot.remove_command('help')
 bot.add_cog(HelpCog(bot))
 bot.add_cog(MusicCog(bot))
 
-# status change
-status = ['Shai is AWSOME!', 'Some music', 'Gabgab is nerd!']
-
-
-
 TOKEN = os.environ.get('BOT_TOKEN')
-bot.run('TOKEN')
+bot.run(TOKEN)
