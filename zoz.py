fc1c2bb1f0a4f958a5882b87be8b5ce88f00b750
ShaiBY10 <Shaibenyosef1@gmail.com>
2022-03-20 21:44:18 +0200

    improve zoz command

--- zoz.py
+++ zoz.py
@@ -7,13 +7,13 @@ PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
 
 imagekit = ImageKit(
-    private_key='private_t1qkmUJbw7rbbOvAzXzdvpVIjuc=',
-    public_key='public_dmcSobM4l5e3s0zt2dzSUJd2hPA=',
+    private_key=PRIVATE_KEY,
+    public_key=PUBLIC_KEY,
     url_endpoint='https://ik.imagekit.io/ShaiTestingPython')
 img_list = []
 
 list_files = imagekit.list_files({"path": "zoz"})
-for i in range(0,len(list_files['response'])):
+for i in range(0, len(list_files['response'])):
     img_list.append(list_files['response'][i]['url'])
 
 pic = random.choice(img_list)
